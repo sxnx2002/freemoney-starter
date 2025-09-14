02ceae160c58906be7364207afbbc7e641b59a86
sxnx2002 <jamehohasd11@gmail.com>
2025-09-14 14:36:52 +0700

    Revert ""Initial commit""
    This reverts commit 0cc6fd29a9c747b80ccac7f0f04ad7bafc8f2520.

--- frontend/utils/api.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios';
-const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_BASE || 'http://localhost:4000' });
-api.interceptors.request.use(cfg=>{ const t = typeof window !== 'undefined' ? localStorage.getItem('token') : null; if(t) cfg.headers.Authorization = 'Bearer ' + t; return cfg; });
-export default api;
